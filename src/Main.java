2a65226513b7c26f87d68c1570356fa4f6f15c73
RichyRichter <like333me@yandex.ru>
2022-02-10 21:13:19 +0400

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 	Task1.writeHello();
+    Task2.calculate();
     }
 }
---
+++ src/Task2.java
@@ -0,0 +1,22 @@
+public class Task2 {
+    public static void calculate(){
+        int firstInt = 114;
+        int secondInt = 24;
+
+        float firstFloat = 459.78F;
+        float secondFloat = 5;
+
+        System.out.println("Операции с int:\n"
+                + firstInt + "+" + secondInt + "=" + (firstInt + secondInt) + "\n"
+                + firstInt + "-" + secondInt + "=" + (firstInt - secondInt) + "\n"
+                + firstInt + "/" + secondInt + "=" + (firstInt / secondInt) + "\n"
+                + firstInt + "%" + secondInt + "=" + (firstInt % secondInt) + "\n"
+                + firstInt + "*" + secondInt + "=" + (firstInt * secondInt) + "\n"
+                + "Операции с float:\n"
+                + firstFloat + "+" + secondFloat + "=" + (firstFloat + secondFloat) + "\n"
+                + firstFloat + "-" + secondFloat + "=" + (firstFloat - secondFloat) + "\n"
+                + firstFloat + "/" + secondFloat + "=" + (firstFloat / secondFloat) + "\n"
+                + firstFloat + "%" + secondFloat + "=" + (firstFloat % secondFloat) + "\n"
+                + firstFloat + "*" + secondFloat + "=" + (firstFloat * secondFloat) + "\n");
+    }
+}
