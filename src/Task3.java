80752614e0452cb2325ddb2d7a2b7212740ce095
RichyRichter <like333me@yandex.ru>
2022-02-10 21:59:44 +0400

    Init commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,15 @@
 public class Task3 {
+    public static void showLogical(){
+        boolean a = true;
+        boolean b = true;
+
+        System.out.println("Операции с boolean:\n" +
+                "a = " + a + " b = " + b + "\n"
+                + "!a: " + !a + "\n"
+                + "a & b: " + (a & b) + "\n"
+                + "a && b: " + (a && b) + "\n"
+                + "a | b: " + (a|b) + "\n"
+                + "a || b: " + (a||b) + "\n"
+                + "a ^ b: " + (a ^ b) + "\n");
+    }
 }
--- src/Task4.java
+++ src/Task4.java
@@ -1,2 +1,15 @@
 public class Task4 {
+    public static void showMassive(){
+        int[] mass = new int[30];
+        for (int i = 0; i < mass.length; i++) {
+
+            mass[i] = i;
+            System.out.print(mass[i] + " ");
+
+            if((i + 1) % 10 == 0)
+            {
+                System.out.println('\n');
+            }
+        }
+    }
 }
