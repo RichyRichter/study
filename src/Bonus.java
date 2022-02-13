3909eeb89d7b4eb95c5ade4908b1dd7be6cf8f1f
RichyRichter <like333me@yandex.ru>
2022-02-13 19:22:08 +0400

    Added alternative solution for bonus

--- src/Bonus.java
+++ src/Bonus.java
@@ -2,4 +2,7 @@ public class Bonus {
     public static boolean checkBoolean(boolean a, boolean b, boolean c, boolean d) {
         return ((a ? 1 : 0) + (b ? 1 : 0) + (c ? 1 : 0) + (d ? 1 : 0)) == 2;
     }
+    public static boolean checkBooleanAlternative(boolean a, boolean b, boolean c, boolean d) {
+        return  a ^ b && c ^ d || c ^ a && d ^ b;
+    }
 }
