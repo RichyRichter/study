e2fafcc4d72502bc9b4a0b326e44587c33848c80
RichyRichter <like333me@yandex.ru>
2022-02-13 17:54:07 +0400

    Main fix

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 public class Main {
 
     public static void main(String[] args) {
+        //раскомментить нужное
+
         //Task1.writeHello();
+
         //Task2.calculate();
+
         //Task3.showLogical();
-        // Task4.showMassive();
+
+        //Task4.showMassive();
+
         //Task5.showNumberOfChar();
-        Task6.charCounter();
+
+        //Task6.charCounter();
+
+        //Task7.countBottles();
+
+        //Task8.calculate();
+
+        //Task9.checkPalindrome();
+
     }
 }
